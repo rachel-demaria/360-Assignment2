14ba559760a24063aa7b175cf90e80557942e7cc
racheldemaria <racheldemaria@100.64.5.58>
2019-10-09 18:03:06 -0700

    Initial master for Assignment 2

---
+++ cse360assign2/src/cse360assign2/AddingMachine.java
@@ -0,0 +1,40 @@
+package cse360assign2;
+
+/**
+ * Performs basic arithmetic on an integer: Total
+ * 
+ * @author Rachel DeMaria
+ * @param value input integer that is to be manipulated mathematically 
+ * @return Total: (String) returns the Total variable in the format of a string
+ * */
+
+public class AddingMachine {
+	
+	private int total;
+	
+	public AddingMachine () {
+		total = 0;  // not needed - included for clarity
+		}
+	
+	public int getTotal () {
+		return 0;
+		}
+	
+	public void add (int value) {
+		
+	}
+	
+	public void subtract (int value) {
+		
+	}
+	
+	public String toString () {
+		return "";
+		}
+	
+	public void clear() {
+		
+	}
+}
+
+
